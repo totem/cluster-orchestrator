1da321fb372c68e4549076c76916265485455268
sukrit007 <sukrit007@gmail.com>
2015-08-12 01:04:40 -0700

    Fix issue with LOG_ROOT_LEVEL

--- conf/appconfig.py
+++ conf/appconfig.py
@@ -3,7 +3,7 @@ import os
 # Logging configuration
 LOG_FORMAT = '%(asctime)s [%(name)s] %(levelname)s %(message)s'
 LOG_DATE = '%Y-%m-%d %I:%M:%S %p'
-LOG_ROOT_LEVEL = os.getenv('LOG_ROOT_LEVEL', 'INFO').upper()
+LOG_ROOT_LEVEL = os.getenv('LOG_ROOT_LEVEL', '').upper() or 'INFO'
 LOG_IDENTIFIER = os.getenv('LOG_IDENTIFIER', 'cluster-orchestrator')
 
 BOOLEAN_TRUE_VALUES = {"true", "yes", "y", "1", "on"}
