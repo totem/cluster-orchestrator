75c20e18cad473b94439fba2d456afda25aa845b
sukrit007 <sukrit007@gmail.com>
2015-11-05 16:32:47 -0700

    Flake8 issues Fix doc

--- orchestrator/jinja/filters.py
+++ orchestrator/jinja/filters.py
@@ -14,8 +14,21 @@ Package that includes custom filters required for totem config processing
 USE_FILTERS = ('replace_regex', )
 
 
-def replace_regex(input, find, replace):
-    """Regex replace filter"""
+def replace_regex(input_str, find, replace):
+    """
+    Regex replace filter that replaces all occurrences of given regex match
+    in a given string
+
+    :param input_str: Input string on which replacement is to be performed
+    :type input_str: str
+    :param find: Regular expression string that needs to be used for
+        find/replacement
+    :type find: str
+    :param replace: Regex replacement string
+    :type replace: str
+    :return: Regex replaced string
+    :rtype: str
+    """
     return re.sub(find, replace, input)
 
 
